1b505ed2dfad5f6383f3d4978dd3e37aa5ea9408
Jo <jnasti101@icloud.com>
2024-03-16 00:55:03 -0400

    sensitive info

--- src2.0/utils/rhoodfuncs.py
+++ src2.0/utils/rhoodfuncs.py
@@ -3,10 +3,12 @@ import robin_stocks.robinhood.options as o
 import robin_stocks.robinhood.stocks  as s
 import pyotp
 import time
+from dotenv import dotenv_values
 
 def login():
-    totp = pyotp.TOTP("R4TFYIX5HPKIHS7R").now()
-    login = r.login("jnasti101@icloud.com", "Hjkl;1234$", mfa_code=totp)
+    values = dotenv_values(".env")
+    totp = pyotp.TOTP(values["TOTP"]).now()
+    login = r.login(values["USERNAME"], values["PASSWORD"], mfa_code=totp)
 
 def getOptionsByDate(name, date):
     options = {}
@@ -55,49 +57,7 @@ def get_price(name):
     price = r.stocks.get_latest_price(name)
     return(float(price[0]))
 
-"""def execute(actions):
-    order_ids = {}
-    for name, amount in actions.items():
-        if(amount < -1):
-            order_ids[name] = sell_crypto_price(name, -amount*.99)
-    for name, amount in actions.items():
-        if(amount > 1):
-            order_ids[name] = buy_crypto_price(name, amount*.99)
-        elif(amount <= 1 and amount >= -1):
-            order_ids[name] = None
-    
-    time.sleep(60*3)
-
-    executed_actions = {}
-    for name, ID in order_ids.items():
-        if(ID == None):
-            print("No order for ", name)
-            executed_actions[name] = 0.0
-        else:
-            info = get_crypto_order_info(ID)
-            if(info['state'] == 'filled'):
-                print("fully executed order for " , name)
-            elif(info['cancel_url'] and float(info['rounded_executed_notional']) > 0):
-                print('partially filled for ', name, "state is", info['state']) 
-                cancel_crypto_order(ID)
-            elif(info['cancel_url'] and float(info['rounded_executed_notional']) == 0):
-                print('order not executed for ', name, "state is", info['state'], "canceling") 
-                cancel_crypto_order(ID)
-            else:
-                print("!!ERROR!! shouldn't ever get here, state is ", info['state'], "execution is ", info["rounded_executions_notional"], "cancel_url is ", info["cancel_url"])
-            executed_actions[name] = float(info['rounded_executed_notional'])
-            if(info['side'] == 'buy'):
-                executed_actions[name] = -executed_actions[name]
-    return(executed_actions)
-
-def sell_crypto_price(name, price):
-    order = r.orders.order_crypto(name , "sell", price, "price")
-    order_id = order['id']
-    return(order_id)
-
-def buy_crypto_price(name, price):
-    order = r.orders.order_crypto(name, "buy", price, "price")
-    order_id = order["id"]
-    return(order_id)
-
-    """
+if __name__ == "__main__":
+    print(dotenv_values(".env"))
+    login()
+    print(get_price("UPRO"))
